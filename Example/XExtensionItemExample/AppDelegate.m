ec5706b0c8354fb0c39b416815431253e2487a26
Bryan Irace <bryan.irace@gmail.com>
2014-12-09 17:52:35 -0500

    Remove excess newlines

--- Example/XExtensionItemExample/AppDelegate.m
+++ Example/XExtensionItemExample/AppDelegate.m
@@ -4,7 +4,6 @@
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    
     self.window.backgroundColor = [UIColor whiteColor];
     self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:
                                       [[ViewController alloc] init]];
--- Example/XExtensionItemExample/ViewController.h
+++ Example/XExtensionItemExample/ViewController.h
@@ -2,4 +2,3 @@
 
 @interface ViewController : UIViewController
 @end
-
