ef03ca7ed61ab43b8e6c58b9537a0454ec3646fa
Bryan Irace <bryan.irace@gmail.com>
2015-05-12 11:34:43 -0400

    More docs and stuff

--- Pod/Classes/XExtensionItem.h
+++ Pod/Classes/XExtensionItem.h
@@ -47,11 +47,11 @@ typedef UIImage *(^XExtensionItemThumbnailProvidingBlock)(CGSize suggestedSize, 
  // receiving app knows to look for those, but this URL will be what activities and extensions receive by default.
  
  NSURL *URL = [NSURL URLWithString:@"http://apple.com/featured"];
+ 
  UIImage *image = [UIImage imageNamed:@"tumblr-featured-on-apple-homepage.png"];
  
  XExtensionItemSource *itemSource = [[XExtensionItemSource alloc] initWithURL:URL];
- [itemSource setAdditionalAttachments:@[image]];
- 
+ itemSource.additionalAttachments = @[image];
  itemSource.title = @"Tumblr featured on Apple.com!";
  itemSource.tags = @[@"tumblr", @"featured", @"so cool"];
  
@@ -74,17 +74,37 @@ typedef UIImage *(^XExtensionItemThumbnailProvidingBlock)(CGSize suggestedSize, 
  An optional string describing the item content. In the case of social activities, for example, this text could be
  shared alongside a URL.
  
+ Calling this setter is analogous to calling `setAttribuetdContentText:forActivityType:` with a `nil` activity type.
+ 
  @see `NSExtensionItem`
  */
 @property (nonatomic, copy) NSAttributedString *attributedContentText;
+
+/**
+ *  <#Description#>
+ *
+ *  @param attributedContentText <#attributedContentText description#>
+ *  @param activityType          <#activityType description#>
+ */
 - (void)setAttributedContentText:(NSAttributedString *)attributedContentText forActivityType:(NSString *)activityType;
 
 /**
  An array of additional media associated with the extension item. These items must be of type `NSString`, `NSURL`,
  `UIImage`, or `NSItemProvider` and will be passed to the selected activity/extension. For example, you could add
  an image attachment to be shared alongside a URL.
+ 
+ Calling this setter is analogous to calling `setAdditionalAttachments:forActivityType:` with a `nil` activity type.
+ 
+ @see `NSExtensionItem`
  */
 @property (nonatomic, copy) NSArray *additionalAttachments;
+
+/**
+ *  <#Description#>
+ *
+ *  @param attachments  <#attachments description#>
+ *  @param activityType <#activityType description#>
+ */
 - (void)setAdditionalAttachments:(NSArray *)attachments forActivityType:(NSString *)activityType;
 
 /**
@@ -129,12 +149,41 @@ typedef UIImage *(^XExtensionItemThumbnailProvidingBlock)(CGSize suggestedSize, 
 
 #pragma mark - Initialization
 
+/**
+ Initialize an item source from a URL.
+ 
+ @param URL (Required) URL to be shared.
+ 
+ @return New item source instance.
+ */
 - (instancetype)initWithURL:(NSURL *)URL;
 
+/**
+ Initialize an item source from a string.
+ 
+ @param text (Required) Text to be shared.
+ 
+ @return New item source instance.
+ */
 - (instancetype)initWithText:(NSString *)text;
 
+/**
+ Initialize an item source from an image.
+ 
+ @param image (Required) Image to be shared.
+ 
+ @return New item source instance.
+ */
 - (instancetype)initWithImage:(UIImage *)image;
 
+/**
+ Initialize an item source from data.
+ 
+ @param data           (Required) Data to be shared.
+ @param typeIdentifier (Required) Type of data.
+ 
+ @return New item source instance.
+ */
 - (instancetype)initWithData:(NSData *)data typeIdentifier:(NSString *)typeIdentifier;
 
 /**
@@ -148,10 +197,15 @@ typedef UIImage *(^XExtensionItemThumbnailProvidingBlock)(CGSize suggestedSize, 
  
  @return New item source instance.
  */
-- (instancetype)initWithPlaceholderItem:(id)placeholderItem typeIdentifier:(NSString *)typeIdentifier itemBlock:(XExtensionItemProvidingBlock)itemBlock NS_DESIGNATED_INITIALIZER;
+- (instancetype)initWithPlaceholderItem:(id)placeholderItem
+                         typeIdentifier:(NSString *)typeIdentifier
+                              itemBlock:(XExtensionItemProvidingBlock)itemBlock NS_DESIGNATED_INITIALIZER;
 
 @end
 
+/**
+ *  A set of convenience initializers that allow items to be provided lazily rather than upfront.
+ */
 @interface XExtensionItemSource (ProviderBlockInitializers)
 
 typedef NSURL *(^XExtensionItemURLProvidingBlock)(NSString *activityType);
@@ -162,14 +216,51 @@ typedef UIImage *(^XExtensionItemImageProvidingBlock)(NSString *activityType);
 
 typedef NSData *(^XExtensionItemDataProvidingBlock)(NSString *activityType);
 
+/**
+ *  <#Description#>
+ *
+ *  @param URLProvider <#URLProvider description#>
+ *
+ *  @return <#return value description#>
+ */
 - (instancetype)initWithURLProvider:(XExtensionItemURLProvidingBlock)URLProvider;
 
+/**
+ *  <#Description#>
+ *
+ *  @param fileURL        <#fileURL description#>
+ *  @param typeIdentifier <#typeIdentifier description#>
+ *
+ *  @return <#return value description#>
+ */
 - (instancetype)initWithFileURLProvider:(XExtensionItemURLProvidingBlock)fileURL typeIdentifier:(NSString *)typeIdentifier;
 
+/**
+ *  <#Description#>
+ *
+ *  @param textProvider <#textProvider description#>
+ *
+ *  @return <#return value description#>
+ */
 - (instancetype)initWithTextProvider:(XExtensionItemStringProvidingBlock)textProvider;
 
+/**
+ *  <#Description#>
+ *
+ *  @param imageProvider <#imageProvider description#>
+ *
+ *  @return <#return value description#>
+ */
 - (instancetype)initWithImageProvider:(XExtensionItemImageProvidingBlock)imageProvider;
 
+/**
+ *  <#Description#>
+ *
+ *  @param dataProvider   <#dataProvider description#>
+ *  @param typeIdentifier <#typeIdentifier description#>
+ *
+ *  @return <#return value description#>
+ */
 - (instancetype)initWithDataProvider:(XExtensionItemDataProvidingBlock)dataProvider typeIdentifier:(NSString *)typeIdentifier;
 
 @end
