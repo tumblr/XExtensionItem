441a5c632e6498f01661a2a8d50c32a6ac596770
Bryan Irace <bryan.irace@gmail.com>
2015-05-15 15:16:10 -0400

    Add comments to sample project

--- Example/App/ViewController.m
+++ Example/App/ViewController.m
@@ -17,9 +17,39 @@
 #pragma mark - Actions
 
 - (void)action {
+    /*
+     An item source must be initialized with a single item. This item’s type will be used to determine which activities 
+     and extensions are displayed; for example, here we’re initializing with a URL so any system activity or third-party 
+     share extension that can take a URL as input will be displayed.
+     
+     You can create an item source out of any raw data so long as you specify its type identifier, but there are 
+     convenience initializers for creating out of a URL, a string, or an image.
+     
+     Additionally, convenient block-based initializers allow you to lazily construct the item once the user has selected 
+     an activity (a placeholder will be used in the interim).
+     */
     XExtensionItemSource *itemSource = [[XExtensionItemSource alloc] initWithURL:[NSURL URLWithString:@"http://apple.com/ipad-air-2/"]];
+    
+    /*
+     If the user selects a system activity that isn’t smart enough to receive structured data (e.g. “Copy”), the item 
+     that your item source was initialized with will simply be passed to it.
+     
+     If the user selects a third-party share extension, however, we’ll pass not only the primary item but also the 
+     following additional attachments.
+     
+     These attachments can be URLs, strings, images, or `NSItemProvider` instances for more advanced functionality 
+     (check out the `NSItemProvider` documentation).
+     */
     itemSource.additionalAttachments = @[@"String of text", [UIImage imageNamed:@"thumbnail"]];
     
+    /*
+     XExtensionItem contains a number of “generic parameters” which are not specific to any share extension. Apps should
+     populate them in order to benefit from extensions that choose to consume them.
+     
+     Some built-in activities (e.g. `UIActivityTypePostToTwitter`) will consume the attributed content text field (if 
+     populated), while others (e.g. “Copy” or “Add to Reading List”) only know how to accept a single attachment. 
+     XExtensionItem is smart enough to handle this for you.
+     */
     itemSource.title = @"Apple";
     itemSource.attributedContentText = [[NSAttributedString alloc] initWithString:@"iPad Air 2. Change is in the air"];
     itemSource.tags = @[@"apple", @"ipad", @"ios"];
@@ -30,6 +60,13 @@
                                                                              webURL:[NSURL URLWithString:@"http://myservice.com/a94ks0583k"]
                                                                           iOSAppURL:[NSURL URLWithString:@"myservice://content/a94ks0583k"]
                                                                       androidAppURL:[NSURL URLWithString:@"myservice://content/a94ks0583k"]];
+    
+    /*
+     Third-party extension developers can add support for custom parameters by creating a class that conforms to 
+     `XExtensionItemCustomParameters` and adding it to XExtensionItem. 
+     
+     Here’s an example of a custom parameters class created by Tumblr.
+     */
     [itemSource addCustomParameters:[[XExtensionItemTumblrParameters alloc] initWithCustomURLPathComponent:@"want-this-for-xmas"
                                                                                          requestedPostType:XExtensionItemTumblrPostTypeLink
                                                                                                consumerKey:@"YOUR_CONSUMER_KEY_HERE"]];
--- Example/Extension/ShareViewController.m
+++ Example/Extension/ShareViewController.m
@@ -1,5 +1,6 @@
 #import "ShareViewController.h"
-#import <XExtensionItem/XExtensionItem+Tumblr.h>
+#import <XExtensionItem/XExtensionItem.h>
+#import <XExtensionItem/XExtensionItemTumblrParameters.h>
 
 @implementation ShareViewController
 
@@ -9,8 +10,16 @@
     [super viewDidLoad];
     
     for (NSExtensionItem *extensionItem in self.extensionContext.inputItems) {
+        /*
+         Loop through the incoming `NSExtensionItem` instances and create an `XExtensionItem` instance out of each.
+         */
         XExtensionItem *xExtensionItem = [[XExtensionItem alloc] initWithExtensionItem:extensionItem];
         
+        NSLog(@"XExtensionItem: %@", xExtensionItem);
+        
+        /*
+         Look through each extension item’s attachments array and load each attachments’ items.
+         */
         for (NSItemProvider *itemProvider in xExtensionItem.attachments) {
             for (NSString *typeIdentifier in itemProvider.registeredTypeIdentifiers) {
                 [itemProvider loadItemForTypeIdentifier:typeIdentifier options:nil completionHandler:^(id <NSSecureCoding> attachmentItem, NSError *error) {
@@ -19,9 +28,19 @@
             }
         }
         
-        NSLog(@"XExtensionItem: %@", xExtensionItem);
-
-        NSLog(@"Tumblr custom parameters: %@", xExtensionItem.tumblrParameters);
+        /*
+         Pull out some generic parameters.
+         */
+        NSLog(@"Tags: %@", xExtensionItem.tags);
+        NSLog(@"Referrer: %@", xExtensionItem.referrer);
+        
+        /*
+         Pull out custom parameter values by initializing a custom parameter class with the extension item’s `userInfo` 
+         dictionary.
+         */
+        XExtensionItemTumblrParameters *tumblrParameters = [[XExtensionItemTumblrParameters alloc] initWithDictionary:xExtensionItem.userInfo];
+        
+        NSLog(@"Tumblr custom URL path component: %@", tumblrParameters.customURLPathComponent);
     }
 }
 
